98bbaa3d99c9a27a13a8b2811e2daab1b4a06eea
John Modica <39076455+CybernetiX-S3C@users.noreply.github.com>
2023-07-06 13:23:00 -0400

    Update create_topology.py

--- create_topology.py
+++ create_topology.py
@@ -1,11 +1,40 @@
 import requests
+import sys
+
+def validate_input(subnets, pcs_per_subnet, mother_ip, subnet_ips):
+  """Validates the input from the user.
+
+  Args:
+    subnets: The number of subnets.
+    pcs_per_subnet: The number of PCs per subnet.
+    mother_ip: The mother IP address.
+    subnet_ips: The IP addresses of each subnet.
+
+  Returns:
+    True if the input is valid, False otherwise.
+  """
+
+  if not isinstance(subnets, int) or subnets < 1:
+    raise ValueError("The number of subnets must be a positive integer.")
+
+  if not isinstance(pcs_per_subnet, int) or pcs_per_subnet < 1:
+    raise ValueError("The number of PCs per subnet must be a positive integer.")
+
+  if not isinstance(mother_ip, str) or not mother_ip.startswith("192.168."):
+    raise ValueError("The mother IP address must be in the format 192.168.xxx.xxx.")
+
+  for subnet_ip in subnet_ips:
+    if not isinstance(subnet_ip, str) or not subnet_ip.startswith("192.168."):
+      raise ValueError("The subnet IP addresses must be in the format 192.168.xxx.xxx.")
+
+  return True
 
 def create_topology(subnets, pcs_per_subnet, mother_ip, subnet_ips):
   """Creates a topology in Cisco Packet Tracer.
 
   Args:
     subnets: The number of subnets.
-    pcs_per_subnet: The number of PCs in each subnet.
+    pcs_per_subnet: The number of PCs per subnet.
     mother_ip: The mother IP address.
     subnet_ips: The IP addresses of each subnet.
 
@@ -13,31 +42,65 @@ def create_topology(subnets, pcs_per_subnet, mother_ip, subnet_ips):
     A URL to the topology in Cisco Packet Tracer.
   """
 
-  url = "https://pt-api.cisco.com/v1/topologies"
-  data = {
-    "name": "Topology",
-    "subnets": subnets,
-    "pcs_per_subnet": pcs_per_subnet,
-    "mother_ip": mother_ip,
-    "subnet_ips": subnet_ips,
-  }
-  response = requests.post(url, json=data)
-  if response.status_code == 200:
-    return response.json()["url"]
-  else:
+  try:
+    if not validate_input(subnets, pcs_per_subnet, mother_ip, subnet_ips):
+      return None
+
+    url = "https://pt-api.cisco.com/v1/topologies"
+    data = {
+      "name": "Topology",
+      "subnets": subnets,
+      "pcs_per_subnet": pcs_per_subnet,
+      "mother_ip": mother_ip,
+      "subnet_ips": subnet_ips,
+    }
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+      return response.json()["url"]
+    else:
+      raise ValueError(response.text)
+  except ValueError as e:
+    print_error_message(e)
     return None
 
-if __name__ == "__main__":
-  subnets = int(input("Enter the number of subnets: "))
-  pcs_per_subnet = int(input("Enter the number of PCs per subnet: "))
-  mother_ip = input("Enter the mother IP address: ")
-  subnet_ips = []
-  for i in range(subnets):
-    subnet_ips.append(input("Enter the IP address of subnet {}: ".format(i + 1)))
+def print_error_message(error_message):
+  """Prints a more detailed error message.
+
+  Args:
+    error_message: The error message to print.
+  """
+
+  print("Failed to create topology.")
+  print(error_message)
+
+def main():
+  """The main function.
+  """
+
+  if len(sys.argv) == 1:
+    print_help_message()
+    return
+
+  command = sys.argv[1]
+  if command == "create":
+    subnets = int(input("Enter the number of subnets: "))
+    pcs_per_subnet = int(input("Enter the number of PCs per subnet: "))
+    mother_ip = input("Enter the mother IP address: ")
+    subnet_ips = []
+    for i in range(subnets):
+      subnet_ips.append(input("Enter the IP address of subnet {}: ".format(i + 1)))
+  elif command == "help":
+    print_help_message()
+    return
+  else:
+    print("Unknown command: {}".format(command))
+    return
+
   url = create_topology(subnets, pcs_per_subnet, mother_ip, subnet_ips)
   if url is not None:
     print("Topology created successfully.")
     print("URL: {}".format(url))
-  else:
-    print("Failed to create topology.")
-    
+
+if __name__ == "__main__":
+  main()
+  
